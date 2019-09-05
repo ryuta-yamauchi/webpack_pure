105335547f558129625d73587e25bca1b565d5ce
Ryuta Yamauchi <r.yamauchi@bitstar.tokyo>
2019-09-05 18:58:47 +0900

    不要なコードを削除

--- spec/system/tops_spec.rb
+++ spec/system/tops_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe "Tops", type: :system do
   describe "#index" do
     it "should image name", js: true do
       visit root_path
-      # expect(page).to have_content("images/ruby.jpg")
       expect(page).to have_css("img")
     end
   end
