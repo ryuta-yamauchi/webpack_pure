b1e2b4d5b689fd7f3048a57479e6b0a5cf0e130f
Ryuta Yamauchi <r.yamauchi@bitstar.tokyo>
2019-09-05 23:14:37 +0900

    Update tops_spec.rb
    it を変更

--- spec/system/tops_spec.rb
+++ spec/system/tops_spec.rb
@@ -3,7 +3,7 @@
 require "rails_helper"
 RSpec.describe "Tops", type: :system do
   describe "#index" do
-    it "should image name", js: true do
+    it "should render image tag", js: true do
       visit root_path
       expect(page).to have_css("img")
     end
